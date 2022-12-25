942abbd78a63a316806f3f43bb58610f1126915e
shoumitro-cse <shoumitro26@gmail.com>
2022-12-26 01:56:49 +0600

    statefull, volume updated

---
+++ StatefulSets/cmd.sh
@@ -0,0 +1,27 @@
+kubectl apply -f StatefulSets/nginx-statefulsets.yaml
+kubectl get statefulset
+kubectl get svc
+
+kubectl describe statefulset.apps/web
+kubectl describe sts web
+
+
+# for volume Persistent Volume Claim
+kubectl get pvc
+kubectl delete pvc pvc-name
+kubectl delete pvc www-web-0
+kubectl describe pvc pvc-name
+
+# delete volume
+kubectl delete pvc www-web-0  www-web-1 www-web-2
+
+# for Persistent Volume
+kubectl get pv
+kubectl describe pv pv-name
+kubectl describe pv example-pv test-volume
+
+# to list all api resource
+kubectl api-resources
+
+# show all storage class
+kubectl get sc
